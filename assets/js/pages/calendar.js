5b826b8a379304273e8dda9680e212957e5b45d9
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:46:10 +0100

    Add renderCalendar function for calendar page

---
+++ assets/js/pages/calendar.js
@@ -0,0 +1,17 @@
+export function renderCalendar(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Calendar</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
