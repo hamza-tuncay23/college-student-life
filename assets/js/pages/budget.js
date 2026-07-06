c8d96d14523b376559d401fb888fa0efcd0f735c
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:43:19 +0100

    Implement renderBudget function for budget page
    Add a renderBudget function to display a coming soon message.

---
+++ assets/js/pages/budget.js
@@ -0,0 +1,17 @@
+export function renderBudget(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Budget</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
