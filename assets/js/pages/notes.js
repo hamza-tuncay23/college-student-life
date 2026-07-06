901be2788e5363f8bcd774008ecb6dc8e77ea099
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:48:02 +0100

    Update and rename assets/js/notes.js to assets/js/pages/notes.js

---
+++ assets/js/pages/notes.js
@@ -0,0 +1,17 @@
+export function renderNotes(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Notes</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
