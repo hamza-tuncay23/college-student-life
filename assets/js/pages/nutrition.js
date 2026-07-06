5541ca0aeabd89d5ae78f14b98a63f320e9d9b7b
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:48:58 +0100

    Create nutrition.js

---
+++ assets/js/pages/nutrition.js
@@ -0,0 +1,17 @@
+export function renderNutrition(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Nutrition</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
