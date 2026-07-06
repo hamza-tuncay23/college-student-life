09ffe9383ccf95322c2cf422ed24ee27051c3e20
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:45:18 +0100

    Update and rename assets/js/wishlist.js to assets/js/pages/wishlist.js

---
+++ assets/js/pages/wishlist.js
@@ -0,0 +1,17 @@
+export function renderWishlist(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Wishlist</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
