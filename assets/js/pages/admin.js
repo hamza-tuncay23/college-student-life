14157cfa9f928b0c4a5cd4fb3e176320cc402951
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:49:47 +0100

    Add renderAdmin function for admin page

---
+++ assets/js/pages/admin.js
@@ -0,0 +1,17 @@
+export function renderAdmin(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Admin</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
