fd2ecaacc8ca0605952a714830ca82dad3725c7d
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:42:47 +0100

    Update and rename assets/js/tasks.js to assets/js/pages/tasks.js

---
+++ assets/js/pages/tasks.js
@@ -0,0 +1,17 @@
+export function renderTasks(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Tasks</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
