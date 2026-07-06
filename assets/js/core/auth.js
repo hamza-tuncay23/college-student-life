625b6bb1c48d3028533b6cd0a9b4df70f04dc3f8
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 20:28:33 +0100

    Implement authentication functions for user login

--- assets/js/core/auth.js
+++ assets/js/core/auth.js
@@ -1,1 +1,57 @@
+// ======================================================
+// College Student Life
+// Authentication
+// ======================================================
 
+import {
+
+    auth
+
+} from "./firebase.js";
+
+import {
+
+    onAuthStateChanged,
+    signOut
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// ======================================================
+
+export function initializeAuth() {
+
+    onAuthStateChanged(auth, (user) => {
+
+        if (user) {
+
+            console.log("Connected :", user.email);
+
+        } else {
+
+            console.log("No user connected.");
+
+        }
+
+    });
+
+}
+
+// ======================================================
+
+export async function logout() {
+
+    try {
+
+        await signOut(auth);
+
+        window.location.href = "login.html";
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+    }
+
+}
