64e0abd1469a6591cd7960759076748c0c4fcc24
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:46:44 +0100

    Update and rename assets/js/drive.js to assets/js/pages/drive.js

---
+++ assets/js/pages/drive.js
@@ -0,0 +1,17 @@
+export function renderDrive(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Drive</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
