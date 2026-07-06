794ee235c64d3c7a7fc332514a2d7faab1678256
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:52:41 +0100

    Create account.js

---
+++ assets/js/pages/account.js
@@ -0,0 +1,17 @@
+export function renderAccount(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Account</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
