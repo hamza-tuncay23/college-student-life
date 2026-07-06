c1bc8bd0aa74e0474ae8c36f39b9288cf1eeafcf
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:51:42 +0100

    Create sport.js

---
+++ assets/js/pages/sport.js
@@ -0,0 +1,17 @@
+export function renderSport(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Sport</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
