f002c4a7e073c94139e497d47b122f7484e438fb
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:50:27 +0100

    Update settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,1 +1,17 @@
+export function renderSettings(){
 
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Settings</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
