05de39fdf86383741bcbba6e90562a4ad1cde234
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:47:31 +0100

    Update and rename assets/js/religion.js to assets/js/pages/religion.js

---
+++ assets/js/pages/religion.js
@@ -0,0 +1,17 @@
+export function renderReligion(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Religion</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
