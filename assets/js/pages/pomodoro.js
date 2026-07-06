8cd18768922eae9b0c0e5e1dc83896b4f490ca07
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:52:11 +0100

    Create pomodoro.js

---
+++ assets/js/pages/pomodoro.js
@@ -0,0 +1,17 @@
+export function renderPomodoro(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Pomodoro</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
