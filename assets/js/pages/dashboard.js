5dd5ecfb0c624550fab023864188d6bbeeb96b7d
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:34:16 +0100

    Add renderDashboard function for dashboard page

---
+++ assets/js/pages/dashboard.js
@@ -0,0 +1,17 @@
+export function renderDashboard() {
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Dashboard</h1>
+
+            <p>
+                Welcome to College Student Life.
+            </p>
+
+        </div>
+
+    `;
+
+}
