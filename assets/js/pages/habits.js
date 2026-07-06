cabe62b3e3d89f237414218fbf7975241fac6461
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-06 19:44:21 +0100

    Add renderHabits function for habits page

---
+++ assets/js/pages/habits.js
@@ -0,0 +1,17 @@
+export function renderHabits(){
+
+    return `
+
+        <div class="comingSoon">
+
+            <h1>Habits</h1>
+
+            <p>
+                This page is under development.
+            </p>
+
+        </div>
+
+    `;
+
+}
