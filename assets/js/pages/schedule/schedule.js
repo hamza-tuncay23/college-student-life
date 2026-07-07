438d3f71269061ddbd92629e8fe45eb10ea9f435
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-07 20:41:12 +0100

    Create schedule.js

---
+++ assets/js/pages/schedule/schedule.js
@@ -0,0 +1,88 @@
+import {
+    renderScheduleGrid,
+    renderSessions,
+    renderUpcomingClass,
+    renderSubjects
+} from "./schedule.ui.js";
+
+import {
+    listenSchedule,
+    getSubjects,
+    getUpcomingClass
+} from "./schedule.firestore.js";
+
+import {
+    initializeScheduleModal
+} from "./schedule.modal.js";
+
+document.addEventListener("DOMContentLoaded", initSchedule);
+
+function initSchedule() {
+
+    // Génère la grille vide
+    renderScheduleGrid();
+
+    // Initialise la fenêtre Ajouter / Modifier
+    initializeScheduleModal();
+
+    // Charge les données Firestore
+    loadSchedule();
+
+    // Bouton +
+    bindEvents();
+
+}
+
+function loadSchedule() {
+
+    listenSchedule((sessions) => {
+
+        renderSessions(sessions);
+
+        renderUpcomingClass(
+            getUpcomingClass(sessions)
+        );
+
+        renderSubjects(
+            getSubjects(sessions)
+        );
+
+    });
+
+}
+
+function bindEvents() {
+
+    // Bouton Ajouter
+    const addButton = document.getElementById("addSessionBtn");
+
+    if (addButton) {
+
+        addButton.addEventListener("click", () => {
+
+            document.dispatchEvent(
+                new CustomEvent("schedule:add")
+            );
+
+        });
+
+    }
+
+    // Recherche
+    const search = document.getElementById("scheduleSearch");
+
+    if (search) {
+
+        search.addEventListener("input", e => {
+
+            document.dispatchEvent(
+                new CustomEvent("schedule:search", {
+                    detail: e.target.value
+                })
+            );
+
+        });
+
+    }
+
+}
