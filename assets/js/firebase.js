ce5c0da5db382a1cbed251b07cf1db6fa6f54fb5
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-08 05:16:50 +0100

    Update and rename assets/js/core/firebase.js to assets/js/firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -3,18 +3,15 @@
 // Firebase Configuration
 // ======================================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// ======================================================
-// Firebase Config
 // ======================================================
 
 const firebaseConfig = {
